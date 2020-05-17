6fa52dcdd3a5786f7111a60be85c8e02fa0de55b
Victor Noagbodji <noagbodjivictor@gmail.com>
2020-05-16 22:56:27 -0400

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "clicked") {
             for (tag of document.getElementsByClassName("togg")) {
-                if (tag.text.slice(0, 2) === "[-") { tag.click(); }
+                if (tag.text.slice(0, 2) === "[–") { tag.click(); }
                 if (parseInt(tag.getAttribute("n")) > 5) { tag.style.color = "red"; }
             }
         }
